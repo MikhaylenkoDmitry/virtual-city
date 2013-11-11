a1fd557e16d7a752d6e1eeb17db7c8b75df9283e
testlnord <testlnord@gmail.com>
2013-11-11 14:52:08 +0400

    initial map generator

---
+++ www/js/mapGenerator.js
@@ -0,0 +1,111 @@
+
+
+this.width = 1000;
+this.height = 1000;
+this.segments = 128;
+this.smoothingFactor  = 64;
+
+function getRandomInt(min, max)
+{
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+this.diamondSquare = function() {
+        this.terrain = new Array();
+
+        for(var i = 0; i <= this.segments; i++) {
+            this.terrain[i] = new Array();
+            for(var j = 0; j <= this.segments; j++) {
+                this.terrain[i][j] = getRandomInt(3,17);
+            }
+        }
+
+		var size = this.segments+1;
+		for(var length = this.segments; length >= 2; length = length /2) {
+			var half = length/2;
+			this.smoothingFactor /= 2;
+
+			// generate the new square values
+			for(var x = 0; x < this.segments; x += length) {
+				for(var y = 0; y < this.segments; y += length) {
+					var average = this.terrain[x][y]+ // top left
+					this.terrain[x+length][y]+ // top right
+					this.terrain[x][y+length]+ // lower left
+					this.terrain[x+length][y+length]; // lower right
+					average /= 4;
+					average += 2*this.smoothingFactor*Math.random()-this.smoothingFactor;
+                    try {
+					this.terrain[x+half][y+half] = average;
+					} catch(e){
+					    alert(x+half);
+					}
+				}
+			}
+
+			// generate the diamond values
+			for(var x = 0; x < this.segments; x += half) {
+				for(var y = (x+half)%length; y < this.segments; y += length) {
+					var average = this.terrain[(x-half+size)%size][y]+ // middle left
+							this.terrain[(x+half)%size][y]+ // middle right
+							this.terrain[x][(y+half)%size]+ // middle top
+							this.terrain[x][(y-half+size)%size]; // middle bottom
+					average /= 4;
+					average += 2*this.smoothingFactor*Math.random()-this.smoothingFactor;
+
+					this.terrain[x][y] = average;
+
+					// values on the top and right edges
+					if(x === 0)
+						this.terrain[this.segments][y] = average;
+					if(y === 0)
+						this.terrain[x][this.segments] = average;
+				}
+			}
+		}
+		this.segments +=1
+		return this.terrain;
+	};
+
+
+
+
+
+function generateMap() {
+
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+    camera.position.z = 1000;
+
+    scene = new THREE.Scene();
+
+    material = new THREE.MeshBasicMaterial( { color: 0x003300, wireframe: true } );
+
+    this.terrain = diamondSquare();
+
+    this.geometry = new THREE.PlaneGeometry(
+        this.width,
+        this.height,
+        this.segments,
+        this.segments
+    );
+    //alert("asdf"+this.geometry.vertices);
+    var index = 0;
+    for(var i = 0; i < this.segments; i++) {
+        for(var j = 0; j < this.segments; j++) {
+                //try{
+                //alert(typeof(terrain));
+
+                this.geometry.vertices[index].setZ(this.terrain[i][j]);
+                //}catch(e){
+                //    alert(index);
+                //}
+                index++;
+
+        }
+    }
+
+
+    mesh = new THREE.Mesh( geometry, material );
+    return mesh;
+}
+
+
