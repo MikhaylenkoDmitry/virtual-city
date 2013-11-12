a542e5aecb6e92e84f997796695af767fbdf487d
Dmitry Mikhaylenko <mih.dmitry@gmail.com>
2013-11-12 13:44:47 +0400

    rotation fixed

---
+++ www/js/wallGenerator.js
@@ -0,0 +1,15 @@
+/**
+* Created by SketchTurner 12.11.13
+**/
+
+function generateCommonWall(scene, x1, z1, x2, z2, width, height)
+{
+    depth = Math.sqrt( (z2-z1)*(z2-z1) + (x2-x1)*(x2-x1) ); // dist between 2 points
+    rad = Math.acos(Math.abs(z2-z1)/depth);
+
+    wall_mesh = generateCommonWallMesh(x1, 0, z1, width, height, depth);
+
+	var axis = new THREE.Vector3(0,1,0);
+	wall_mesh.rotateOnAxis(axis, rad);
+	scene.add(wall_mesh);
+}
