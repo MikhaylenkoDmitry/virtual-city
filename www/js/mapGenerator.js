ae7ee4abe64d7e49420edef5446fd43954c03e87
testlnord <testlnord@gmail.com>
2013-11-13 14:36:21 +0400

    fix

--- www/js/mapGenerator.js
+++ www/js/mapGenerator.js
@@ -362,7 +362,7 @@ function placeStuff(scene){
     var zm_y_0 = zm[1];
     var zm_angle = Math.random()*Math.PI;
     giveMeTheTower(scene,zm_x_0,0,zm_y_0,zm_size,zm_size*2,zm_size, zm_angle);
-    this.house_coords(new Vertex(zm_x_0,0,zm_y_0));
+    this.house_coords.push(new THREE.Vector3(zm_x_0,0,zm_y_0));
     //городская стена
     cityWall(zm_x_0, zm_y_0, 700);
 
@@ -384,7 +384,7 @@ function placeStuff(scene){
             if (z< min_h) min_h = z;
 
             generateBuilding(scene, x,z,y , 10, 10, Math.random()*Math.PI);
-            this.house_coords.push( new Vertex(x,z,y));
+            this.house_coords.push( new THREE.Vector3(x,z,y));
         }
 
     }
