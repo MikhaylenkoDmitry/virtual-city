d68286f06dc11b546c3a7ef8036701b1efc0b5d5
Nick <dopehate@gmail.com>
2013-11-13 12:13:45 +0300

    added Skybox

--- www/js/wallGenerator.js
+++ www/js/wallGenerator.js
@@ -6,6 +6,7 @@ function generateCommonWall(scene, x1, z1, x2, z2, width, height)
 {
     var depth = Math.sqrt( (z2-z1)*(z2-z1) + (x2-x1)*(x2-x1) ); // dist between 2 points
     var rad = Math.acos(Math.abs(z2-z1)/depth);
+    //var rad = Math.atan(Math.abs(z2-z1)/depth);
 
     var wall_mesh = generateCommonWallMesh(0, 0, 0, width, height, depth);
 
